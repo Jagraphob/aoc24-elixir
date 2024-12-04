6a7f4a0fd5aedc89fe44e38645337e5697839f0e
Toe <toe15g@gmail.com>
2024-12-04 22:56:17 +1300

    mix new project

---
+++ lib/advent24_elixir.ex
@@ -0,0 +1,18 @@
+defmodule Advent24Elixir do
+  @moduledoc """
+  Documentation for `Advent24Elixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Advent24Elixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/advent24_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule Advent24ElixirTest do
+  use ExUnit.Case
+  doctest Advent24Elixir
+
+  test "greets the world" do
+    assert Advent24Elixir.hello() == :world
+  end
+end
